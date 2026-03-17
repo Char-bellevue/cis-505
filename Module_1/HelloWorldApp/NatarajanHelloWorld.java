8d54348cf02a0fc3d0d0dae29d71bb6e14c430d5
Charanya <cnatarajan@my365.bellevue.edu>
2026-03-17 18:25:28 -0500

    Hello world assignment

---
+++ Module_1/HelloWorldApp/NatarajanHelloWorld.java
@@ -0,0 +1,8 @@
+public class NatarajanHelloWorld {
+    public static void main(String[] args) {
+        String firstName = "Charanya";
+        String lastName = "Natarajan";
+
+        System.out.printf("Hello World from %s %s.%n", firstName, lastName);
+    }
+}
